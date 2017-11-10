30184951fe12303cbf5f0dca2451cd8ba994e9e0
Adarsh Pavani <adarsh.pavani@mapbox.com>
2017-11-09 18:37:26 -0800

    batchmode and pathfix

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo /Applications/Unity/Unity.app/Contents/MacOS/Unity -projectpath ./sdkproject -quit -batchmode -executeMethod CreateBuild.BuildNow
+sudo /Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -projectPath "${WORKSPACE}" -quit -batchmode -executeMethod CreateBuild.BuildNow
