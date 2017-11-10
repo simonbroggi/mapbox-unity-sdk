a7f46f16cc092305e88508d46916222022a1b87a
Adarsh Pavani <adarsh.pavani@mapbox.com>
2017-11-09 17:02:35 -0800

    createbuild jenkins push

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 /Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -executeMethod CreateBuild.BuildNow
+zip -r Build.zip Build
--- sdkproject/Assets/Editor/CreateBuild.cs
+++ sdkproject/Assets/Editor/CreateBuild.cs
@@ -6,7 +6,7 @@ public class CreateBuild
 	{
 		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
 		buildPlayerOptions.scenes = new[] { "Assets/Mapbox/Examples/VectorDataVisualization/VectorDataVisualization.unity"};
-		buildPlayerOptions.locationPathName = "../../Build/OSXBuild";
+		buildPlayerOptions.locationPathName = "../Build/OSXBuild";
 		buildPlayerOptions.target = BuildTarget.StandaloneOSXIntel64;
 		buildPlayerOptions.options = BuildOptions.None;
 		BuildPipeline.BuildPlayer(buildPlayerOptions);
