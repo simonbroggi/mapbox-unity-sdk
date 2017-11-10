fa9a43226b2c47c67aa39a1b6061131f80be89dc
Adarsh Pavani <adarsh.pavani@mapbox.com>
2017-11-09 23:19:05 -0800

    jenkins works.. yaay

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-sudo /Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -projectPath "${WORKSPACE}" -quit -batchmode -executeMethod CreateBuild.BuildNow
+sudo su jenkins
+./update-mapbox-unity-sdk-core.sh
+sudo /Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -projectPath "${WORKSPACE}/sdkproject" -quit -batchmode -executeMethod CreateBuild.BuildNow -stackTraceLogType Full -logFile "MapboxBuildLog.txt"
+sudo cp  -r "${WORKSPACE}/Build/" "${JENKINS_HOME}/jobs/${JOB_NAME}/builds/${BUILD_NUMBER}"
+sudo rm -rf "${WORKSPACE}/Build/"
