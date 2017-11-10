107780bc7adfd22b8bef090f8bb5ecb39cd6102e
Adarsh Pavani <adarsh.pavani@mapbox.com>
2017-11-09 17:41:11 -0800

    Revert "no pwd requirement for jenkins. assigning sudo permissions"
    This reverts commit aebb8e214b2c8e0edc215dc72e7c6edfa67f198c.

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-jenkins ALL=NOPASSWD: /bin/sh, build.sh
-sudo /Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -executeMethod CreateBuild.BuildNow
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -executeMethod CreateBuild.BuildNow
 zip -r Build.zip Build
