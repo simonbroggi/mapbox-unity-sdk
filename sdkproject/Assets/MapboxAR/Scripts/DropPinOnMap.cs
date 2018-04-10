ed447734834d508a42cd029a0d794c5238bf5551
lauri-rustanius <lauri.rustanius@mapbox.com>
2018-04-10 23:49:40 +0300

    drop pin on map example

---
+++ sdkproject/Assets/MapboxAR/Scripts/DropPinOnMap.cs
@@ -0,0 +1,46 @@
+﻿namespace Mapbox.Examples
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using Mapbox.Unity.Map;
+	using UnityEngine.UI;
+
+	public class DropPinOnMap : MonoBehaviour
+	{
+		[SerializeField]
+		AbstractMap _map;
+
+		[SerializeField]
+		Camera _cam;
+
+		[SerializeField]
+		GameObject _pin;
+
+		private void Start()
+		{
+
+		}
+
+		void Update()
+		{
+			if (Input.GetMouseButtonDown(0) && _cam.rect.y >= 0)
+			{
+				Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+				RaycastHit hit;
+
+				if (Physics.Raycast(ray, out hit))
+				{
+					Debug.Log(hit.collider.gameObject.name);
+					Instantiate(_pin, hit.point, Quaternion.identity, _map.gameObject.transform);
+				}
+			}
+		}
+
+		public void SetCameraViewPort()
+		{
+			var rectVal = _cam.rect.y >= 0f ? -.66f : 0;
+			_cam.rect = new Rect(_cam.rect.x, rectVal, _cam.rect.width, _cam.rect.height);
+		}
+	}
+}
---
+++ sdkproject/Assets/MapboxAR/Scripts/PinMapButton.cs
@@ -0,0 +1,43 @@
+﻿namespace Mapbox.Examples
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	public class PinMapButton : MonoBehaviour
+	{
+		[SerializeField]
+		Button _button;
+
+		[SerializeField]
+		ManualTouchCamera _touchCam;
+
+		[SerializeField]
+		CameraMovement _touchOnPinMap;
+
+		bool _toggle;
+
+		void Start()
+		{
+			_toggle = true;
+			_button.onClick.AddListener(ToggleMadness);
+		}
+
+		void ToggleMadness()
+		{
+			_toggle = _toggle == true ? false : true;
+
+			_touchCam.enabled = _toggle;
+
+			if (_toggle)
+			{
+				_touchOnPinMap.enabled = false;
+			}
+			else
+			{
+				_touchOnPinMap.enabled = true;
+			}
+		}
+	}
+}
