c09180e76c067cea3d35b3790cdde69724546658
lauri-rustanius <lauri.rustanius@mapbox.com>
2018-04-11 23:40:21 +0300

    added world anchor point example

---
+++ sdkproject/Assets/MapboxAR/Scripts/Examples/StaticMapReference.cs
@@ -0,0 +1,61 @@
+﻿namespace Mapbox.Examples
+{
+	using UnityEngine;
+	using Mapbox.Unity.Map;
+	using System;
+
+	public class StaticMapReference : MonoBehaviour
+	{
+		[SerializeField]
+		AbstractMap _map;
+
+		[SerializeField]
+		bool _dontDestroyOnLoad;
+
+		public Action MapInitialized;
+
+		private static StaticMapReference _instance;
+		public static StaticMapReference Instance
+		{
+			get
+			{
+				return _instance;
+			}
+
+			private set
+			{
+				_instance = value;
+			}
+		}
+
+		private void Awake()
+		{
+			if (Instance != null)
+			{
+				DestroyImmediate(gameObject);
+				return;
+			}
+
+			Instance = this;
+
+			Action handler = null;
+			handler = () =>
+			{
+				MapInitialized();
+				_map.OnInitialized -= handler;
+			};
+
+			_map.OnInitialized += handler;
+
+			if (_dontDestroyOnLoad)
+			{
+				DontDestroyOnLoad(gameObject);
+			}
+		}
+
+		public AbstractMap ReturnMap()
+		{
+			return _map;
+		}
+	}
+}
---
+++ sdkproject/Assets/MapboxAR/Scripts/Examples/WorldAnchorPoint.cs
@@ -0,0 +1,29 @@
+﻿namespace Mapbox.Examples
+{
+	using UnityEngine;
+	using Mapbox.Utils;
+
+	public class WorldAnchorPoint : MonoBehaviour
+	{
+
+		[SerializeField]
+		double _latitude;
+
+		[SerializeField]
+		double _longitude;
+
+		Vector2d _latLon;
+
+		void Start()
+		{
+			StaticMapReference.Instance.MapInitialized += SetWorldPoint;
+		}
+
+		void SetWorldPoint()
+		{
+			_latLon = new Vector2d(_latitude, _longitude);
+			var map = StaticMapReference.Instance.ReturnMap();
+			transform.position = map.GeoToWorldPosition(_latLon);
+		}
+	}
+}
