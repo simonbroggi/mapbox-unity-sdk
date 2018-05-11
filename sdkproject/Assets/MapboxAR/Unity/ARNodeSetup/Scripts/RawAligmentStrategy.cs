b0c366f605ba9960eec343b3862c62dc64f53009
lauri-rustanius <lauri.rustanius@mapbox.com>
2018-05-11 23:53:38 +0300

    added raw aligment strategy with no lerping

---
+++ sdkproject/Assets/MapboxAR/Unity/ARNodeSetup/Scripts/RawAligmentStrategy.cs
@@ -0,0 +1,32 @@
+﻿namespace Mapbox.Unity.Ar
+{
+	using UnityEngine;
+	using Mapbox.Unity.Map;
+	using Mapbox.Utils;
+
+	public class RawAligmentStrategy : AbstractAlignmentStrategy
+	{
+		[SerializeField]
+		AbstractMap _map;
+		bool _isInitialized;
+
+		private void Awake()
+		{
+			_map.OnInitialized += () => _isInitialized = true;
+		}
+
+		public override void OnAlignmentAvailable(Alignment alignment)
+		{
+			if (_isInitialized)
+			{
+				var latlon = _map.WorldToGeoPosition(alignment.Position);
+				_map.UpdateMap(latlon, _map.Zoom);
+				_map.transform.position = new Vector3(_map.transform.position.x, alignment.Position.y, _map.transform.position.z);
+				var rotation = Quaternion.Euler(0, alignment.Rotation, 0);
+				var inverse = Quaternion.Inverse(rotation);
+				_map.transform.SetPositionAndRotation(_transform.position, inverse);
+			}
+		}
+	}
+}
+
