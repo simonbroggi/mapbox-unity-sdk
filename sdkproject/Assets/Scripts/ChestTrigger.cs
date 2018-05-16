9c8458651123bf6ac336d356c18b8099437f0dc7
Adarsh Pavani <adarsh.pavani@mapbox.com>
2018-05-15 18:51:09 -0700

    final state of scene

--- sdkproject/Assets/Scripts/ChestTrigger.cs
+++ sdkproject/Assets/Scripts/ChestTrigger.cs
@@ -1,15 +1,29 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ChestTrigger : MonoBehaviour {
 
+	private string[] particles = { "SparksDot", "SparksMain", "SparksStar", "model" };
 	private void OnTriggerEnter(Collider other)
 	{
 		if(other.tag=="Player")
 		{
 			Debug.Log("Open Chest");
 			GetComponent<Animator>().Play("Open");
+			StopAllCoroutines();
+			StartCoroutine(OpenChest());
+		}
+	}
+
+	private IEnumerator OpenChest()
+	{
+		yield return new WaitForSeconds(1f);
+		foreach (var particle in particles)
+		{
+			transform.Find(particle).gameObject.SetActive(true);
+			yield return null;
 		}
 	}
 
@@ -17,8 +31,12 @@ public class ChestTrigger : MonoBehaviour {
 	{
 		if(other.tag=="Player")
 		{
-			Debug.Log("Close Chest");
-			GetComponent<Animator>().Play("Close");
+			Debug.Log("Closed Chest");
+			GetComponent<Animator>().Play("Closed");
+			foreach (var particle in particles)
+			{
+				transform.Find(particle).gameObject.SetActive(false);
+			}
 		}
 	}
 }
--- sdkproject/Assets/Scripts/PlayerController.cs
+++ sdkproject/Assets/Scripts/PlayerController.cs
@@ -5,12 +5,25 @@ public class PlayerController : MonoBehaviour
 	public float translateSpeed = 100f;
 	public float rotateSpeed = 100f;
 
+	private Animator animator;
+	private void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
 	void Update()
 	{
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * translateSpeed;
-		var z = Input.GetAxis("Vertical") * Time.deltaTime * rotateSpeed;
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * translateSpeed;
 
 		transform.Rotate(0, x, 0);
-		transform.Translate(0, 0, z);
+		if (z > 0)
+		{
+			transform.Translate(0, 0, z);
+			animator.SetBool("IsRunning", true);
+		}
+		else
+		{
+			animator.SetBool("IsRunning", false);
+		}
 	}
 }
