779e6d7d37a1e84a6daac13e87460e0f13d8f0af
lauri-rustanius <lauri.rustanius@mapbox.com>
2018-06-06 17:10:43 -0700

    ARLocalizationStrategy.cs cleanup

--- sdkproject/Assets/world-eval-setup/ARNodeSetup/Scripts/ARLocalizationStrategy.cs
+++ sdkproject/Assets/world-eval-setup/ARNodeSetup/Scripts/ARLocalizationStrategy.cs
@@ -18,33 +18,19 @@
 		[SerializeField]
 		MiniMapContoller _miniMap;
 
-		[SerializeField]
-		float _updateHeadingInterval = 10;
-
-		Vector3 _mapMatchNode;
-		float _timeToUpdateHeading;
-
 		ARInterface.CustomTrackingState _trackingState;
 		ARInterface _arInterface;
-		bool _isHeadingCached;
-		float _planePosOnY = -.5f;
 		Transform _arFpsCamera;
-		float _cacheHeading;
+
+		float _previousAccuracy, _previousHeading;
+
 		NodeSyncBase _gpsNodeSync, _arNodeSync, _mapMatchingNode;
 
 		private void Start()
 		{
-			//Well this should be here...
 			_arInterface = ARInterface.GetInterface();
 			_trackingState = new ARInterface.CustomTrackingState();
 			_arFpsCamera = CentralizedLocator.Instance.ArFirstPerson;
-			//Not sure about this.. Should it also... I need to place the map..
-			//Because were not really anymore updating the map...
-			//Guess not.. maan what the fuck am I doing?.
-			ARInterface.planeAdded += GetPlaneCoords;
-			ARInterface.planeUpdated += GetPlaneCoords;
-			_timeToUpdateHeading = _updateHeadingInterval;
-
 
 			//Awful
 			foreach (var nodeBase in CentralizedLocator.Instance.SyncNodes)
@@ -71,104 +57,41 @@
 			var firstPersonPos = centralizedARLocator.ArFirstPerson;
 			var location = LocationProviderFactory.Instance.DefaultLocationProvider.CurrentLocation;
 
-			//Bascically 
+
+			// Check gps if it's better, then check if heading is better, then if none of those are better then 
+			// use only ar or gps.
+
+			// Basic strategy
 			if (CheckTracking())
 			{
 				aligment.Position = firstPersonPos.position;
 				aligment.LatLon = map.WorldToGeoPosition(firstPersonPos.position);
 				aligment.Rotation = firstPersonPos.eulerAngles.y;
 
-				Debug.Log(aligment.LatLon);
-
-				if (_timeToUpdateHeading <= 0)
+				if (_gpsNodeSync.ReturnLatestNode().Accuracy <= _previousAccuracy)
 				{
-					aligment.Rotation = _heading.ReturnAverage();
-					_cacheHeading = _heading.ReturnAverage();
-					_isHeadingCached = true;
-					Unity.Utilities.Console.Instance.Log(string.Format
-														 ("New average heading: {0}", aligment.Rotation)
-														 , "red");
-
-					_timeToUpdateHeading = _updateHeadingInterval;
+					aligment.Position = map.GeoToWorldPosition(_gpsNodeSync.ReturnLatestNode().LatLon, false);
+					aligment.LatLon = _gpsNodeSync.ReturnLatestNode().LatLon;
 				}
 
-				Unity.Utilities.Console.Instance.Log(string.Format("Keeping at Location")
-														 , "green");
-				OnLocalizationComplete(aligment);
-
-				return;
-			}
-
-			// FOR MapMatching Node..
-
-			if (CanSnatchMapMatchingNode(centralizedARLocator, ref _mapMatchNode) && _gpsNodeSync.ReturnLatestNode().Accuracy <= 20)
-			{
-				Unity.Utilities.Console.Instance.Log(string.Format("Snatched MapMatchNode: {0}", _mapMatchNode)
-					, "yellow"
-				);
-
-				aligment.Position = _mapMatchNode;
-				//aligment.Rotation = averageHeading;
-
-				Unity.Utilities.Console.Instance.Log(string.Format("Aligning map by MapMatchingNode")
-					, "yellow"
-				);
+				if (location.IsUserHeadingUpdated)
+				{
+					aligment.Rotation = location.UserHeading;
+					Unity.Utilities.Console.Instance.Log(string.Format
+												 ("New UserHeading heading: {0}", aligment.Rotation)
+												 , "red");
+				}
 
 				OnLocalizationComplete(aligment);
 				return;
 			}
 
-			//GPS
+			// GPS
 			aligment.Position = map.GeoToWorldPosition(location.LatitudeLongitude, false);
 			aligment.LatLon = location.LatitudeLongitude;
-			aligment.Rotation = _heading.ReturnAverage();
+			aligment.Rotation = location.DeviceOrientation;
 			OnLocalizationComplete(aligment);
 
-		}
-
-		float ReturnPlayerAngle()
-		{
-			Vector3 targetPos = new Vector3(_arFpsCamera.position.x, _arFpsCamera.position.y - 1f, _arFpsCamera.position.z);
-			Vector3 targetDir = targetPos - _arFpsCamera.position;
-			return Vector3.Angle(targetDir, _arFpsCamera.forward);
-		}
-
-		private void Update()
-		{
-			_timeToUpdateHeading -= Time.deltaTime;
-
-
-			if (ReturnPlayerAngle() >= 70)
-			{
-				_miniMap.SetMapMode(Mode.AR);
-			}
-
-			if (ReturnPlayerAngle() < 50)
-			{
-				_miniMap.SetMapMode(Mode.TopDown);
-			}
-		}
-
-		void GetPlaneCoords(BoundedPlane plane)
-		{
-			_planePosOnY = plane.center.y;
-		}
-
-		bool CanSnatchMapMatchingNode(CentralizedLocator arLocator, ref Vector3 vector3)
-		{
-			foreach (var nodebase in arLocator.SyncNodes)
-			{
-				if (nodebase.GetType() == typeof(ARMapMatching))
-				{
-					if (nodebase.ReturnNodes().Count() != 0)
-					{
-						var nodes = nodebase.ReturnNodes();
-						vector3 = arLocator.CurrentMap.GeoToWorldPosition(nodes[0].LatLon, false);
-						return true;
-					}
-				}
-			}
-			return false;
 		}
 
 		bool CheckTracking()
