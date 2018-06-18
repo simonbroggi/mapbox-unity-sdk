5016eaeb1d3f33905b4b2df1a51adf1ec575b7e0
jim-martin <jim.martin@mapbox.com>
2018-06-18 12:19:35 -0700

    simple distance check for replacefeature modifier

--- sdkproject/Assets/Mapbox/Unity/MeshGeneration/Modifiers/MeshModifiers/ReplaceFeatureModifier.cs
+++ sdkproject/Assets/Mapbox/Unity/MeshGeneration/Modifiers/MeshModifiers/ReplaceFeatureModifier.cs
@@ -16,75 +16,43 @@
 	[CreateAssetMenu(menuName = "Mapbox/Modifiers/Replace Feature Modifier")]
 	public class ReplaceFeatureModifier : GameObjectModifier, IReplacementCriteria
 	{
-		private List<string> _latLonToSpawn;
-
+		//object pool
 		private Dictionary<ulong, GameObject> _objects;
 		private GameObject _poolGameObject;
+
+		//spawn settings
 		[SerializeField]
 		private SpawnPrefabOptions _options;
 		private List<GameObject> _prefabList = new List<GameObject>();
+		private List<string> _latLonToSpawn;
 
 		[SerializeField]
 		[Geocode]
 		private List<string> _prefabLocations;
 
+		//maximum distance to trigger feature replacement ( in tile space ), 
+		// !! this changes depending on the latitude of the replacement
 		[SerializeField]
-		private List<string> _explicitlyBlockedFeatureIds;
-		//maximum distance to trigger feature replacement ( in tile space )
-		private const float _maxDistanceToBlockFeature_tilespace = 1000f;
-
-		/// <summary>
-		/// List of featureIds to test against. 
-		/// We need a list of featureIds per location. 
-		/// A list is required since buildings on tile boundary will have multiple id's for the same feature.
-		/// </summary>
-		private List<List<string>> _featureId;
-		private string _tempFeatureId;
+		private const float _replacementRange = 500f;
 
 		public override void Initialize()
 		{
 			base.Initialize();
+
 			//duplicate the list of lat/lons to track which coordinates have already been spawned
 			_latLonToSpawn = new List<string>(_prefabLocations);
-			_featureId = new List<List<string>>();
-			for (int i = 0; i < _prefabLocations.Count; i++)
-			{
-				_featureId.Add(new List<string>());
-			}
+
+			//object pool
 			if (_objects == null)
 			{
 				_objects = new Dictionary<ulong, GameObject>();
 				_poolGameObject = new GameObject("_inactive_prefabs_pool");
 			}
-			_latLonToSpawn = new List<string>(_prefabLocations);
 		}
 
 		public override void SetProperties(ModifierProperties properties)
 		{
 			_options = (SpawnPrefabOptions)properties;
-		}
-
-		public override void FeaturePreProcess(VectorFeatureUnity feature)
-		{
-			int index = -1;
-			foreach (var point in _prefabLocations)
-			{
-				try
-				{
-					index++;
-					var coord = Conversions.StringToLatLon(point);
-					if (feature.ContainsLatLon(coord) && (feature.Data.Id != 0))
-					{
-						_featureId[index] = (_featureId[index] == null) ? new List<string>() : _featureId[index];
-						_tempFeatureId = feature.Data.Id.ToString();
-						_featureId[index].Add(_tempFeatureId.Substring(0, _tempFeatureId.Length - 3));
-					}
-				}
-				catch (Exception e)
-				{
-					Debug.LogException(e);
-				}
-			}
 		}
 
 		/// <summary>
@@ -94,38 +62,17 @@
 		/// <param name="feature">Feature.</param>
 		public bool ShouldReplaceFeature(VectorFeatureUnity feature)
 		{
-			int index = -1;
-
-			foreach (var point in _prefabLocations)
+			for (int i = 0; i < _prefabLocations.Count; i++)
 			{
+				var point = _prefabLocations[i];
+
 				try
 				{
-					index++;
-					if (_featureId[index] != null)
+					var from = Conversions.LatitudeLongitudeToVectorTilePosition(Conversions.StringToLatLon(point), feature.Tile.InitialZoom);
+					var to = feature.Data.Geometry<float>()[0][0];
+					if (Vector2.SqrMagnitude(new Vector2(from.x, from.y) - new Vector2(to.X, to.Y)) < Math.Pow(_replacementRange, 2f))
 					{
-						foreach (var featureId in _featureId[index])
-						{
-							//preventing spawning of explicitly blocked features
-							foreach (var blockedId in _explicitlyBlockedFeatureIds)
-							{
-								if (feature.Data.Id.ToString() == blockedId)
-								{
-									return true;
-								}
-							}
-
-							var from = Conversions.LatitudeLongitudeToVectorTilePosition(Conversions.StringToLatLon(point), feature.Tile.InitialZoom);
-							var to = feature.Data.Geometry<float>()[0][0];
-							if( Vector2.SqrMagnitude( new Vector2(from.x, from.y) - new Vector2(to.X, to.Y)) > Math.Pow(_maxDistanceToBlockFeature_tilespace, 2f))
-							{
-								return false;
-							}
-
-							if (feature.Data.Id.ToString().StartsWith(featureId, StringComparison.CurrentCulture))
-							{
-								return true;
-							}
-						}
+						return true;
 					}
 				}
 				catch (Exception e)
@@ -134,6 +81,38 @@
 				}
 
 			}
+			return false;
+		}
+
+		/// <summary>
+		/// Checks if the feature should be used to spawn a prefab, once per lat/lon
+		/// </summary>
+		/// <returns><c>true</c>, if the feature should be spawned <c>false</c> otherwise.</returns>
+		/// <param name="feature">Feature.</param>
+		private bool ShouldSpawnFeature(VectorFeatureUnity feature)
+		{
+			//don't spawn if the feature isn't valid
+			if (feature == null)
+			{
+				return false;
+			}
+
+			//spawn the feature if it's already in the object pool
+			if (_objects.ContainsKey(feature.Data.Id))
+			{
+				return true;
+			}
+
+			foreach (var point in _latLonToSpawn)
+			{
+				var coord = Conversions.StringToLatLon(point);
+				if (feature.ContainsLatLon(coord))
+				{
+					_latLonToSpawn.Remove(point);
+					return true;
+				}
+			}
+
 			return false;
 		}
 
@@ -210,35 +189,6 @@
 			}
 		}
 
-		/// <summary>
-		/// Checks if the feature should be used to spawn a prefab, once per lat/lon
-		/// </summary>
-		/// <returns><c>true</c>, if the feature should be spawned <c>false</c> otherwise.</returns>
-		/// <param name="feature">Feature.</param>
-		private bool ShouldSpawnFeature(VectorFeatureUnity feature)
-		{
-			if (feature == null)
-			{
-				return false;
-			}
-
-			if (_objects.ContainsKey(feature.Data.Id))
-			{
-				return true;
-			}
-
-			foreach (var point in _latLonToSpawn)
-			{
-				var coord = Conversions.StringToLatLon(point);
-				if (feature.ContainsLatLon(coord))
-				{
-					_latLonToSpawn.Remove(point);
-					return true;
-				}
-			}
-
-			return false;
-		}
 		public override void OnPoolItem(VectorEntity vectorEntity)
 		{
 			base.OnPoolItem(vectorEntity);
